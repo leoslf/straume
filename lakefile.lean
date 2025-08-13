9e09d80b25e3beb8f9ff1e8260ed8e383cf6a21e
Leo Sin <leo.sin@my.cityu.edu.hk>
2025-08-12 21:18:49 -0400

    build: fix build issue

--- lakefile.lean
+++ lakefile.lean
@@ -6,13 +6,13 @@ package Straume
 lean_lib Straume
 
 require batteries from git
-  "https://github.com/leanprover-community/batteries" @ "v4.21.0"
+  "https://github.com/leanprover-community/batteries" @ "v4.22.0-rc4"
 
 require LSpec from git
   "https://github.com/argumentcomputer/LSpec" @ "24cceb69c20fadca0fd3acabe39fa9270dfb47e6"
 
 require YatimaStdLib from git
-  "https://github.com/leoslf/YatimaStdLib.lean" @ "1fdc0d2513d991570f5d0ddeb55569f81e0859e7"
+  "https://github.com/leoslf/YatimaStdLib.lean" @ "c9439e98d445a55ae9b9397631971883c715063d"
 
 @[default_target]
 lean_exe straume where
